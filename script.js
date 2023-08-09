69530bf0cd0d4aacb962edf4d9ecd71e4dddc05d
rishabhdiwaker <rishabhdiwaker.rd@gmail.com>
2023-08-09 23:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0;
+	library.forEach((ele,index) => {
+		if(ele.readingStatus == true){
+			count++;
+		}
+	})
+	return count;
 };
 
 // Do not change the code below
